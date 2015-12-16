3276ae341512859acb15ccc6fbb381b080d8d097
MarijnBrosens <marijnbrosens16@hotmail.com>
2015-12-16 04:05:02 +0100

    + partials/nav

---
+++ code/resources/views/partials/nav.blade.php
@@ -0,0 +1,20 @@
+<ul class="nav">
+    <li><a href="/watchlist" >icon WATCHLIST</a></li>
+    <li><a href="/profile">icon PROFILE</a></li>
+    <li><a href="/login">LOGIN</a></li>
+    <input type="search">
+    <button>search icon</button>
+</ul>
+
+<ul class="nav">
+    <li><a href="/" >HOME</a></li>
+    <li><a href="/art">ART</a></li>
+    <li><a href="/isearch">ISEARCH</a></li>
+    <li><a href="/auctions">MYAUCTIONS</a></li>
+    <li><a href="/bids">MYBIDS</a></li>
+    <li><a href="/contact">CONTACT</a></li>
+
+    <li><a href="/nl">NL</a></li>
+    <li><a href="/fr">FR</a></li>
+    <li><a href="/en">EN</a></li>
+</ul>
